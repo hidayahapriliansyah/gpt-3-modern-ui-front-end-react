5ec959b809e93e7a0636d7ba726e18d6ada95b47
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-24 11:42:21 +0700

    navbar finish

--- gpt3-jsm/src/App.js
+++ gpt3-jsm/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
+import './App.css';
+
 const App = () => {
   return (
     <div className='App'>
